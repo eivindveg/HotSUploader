bcaabf7604691a7efa5871df36e26522da8de9b6
eivindveg <eivindveg@hotmail.com>
2015-04-02 03:01:03 +0200

    Add basic window with no functions

--- src/main/java/com/metacodestudio/hotsuploader/Client.java
+++ src/main/java/com/metacodestudio/hotsuploader/Client.java
@@ -1,6 +1,13 @@
 package com.metacodestudio.hotsuploader;
 
+import com.metacodestudio.hotsuploader.controllers.HomeController;
+import io.datafx.controller.flow.Flow;
+import io.datafx.controller.flow.FlowHandler;
+import io.datafx.controller.flow.container.DefaultFlowContainer;
+import io.datafx.controller.flow.context.ViewFlowContext;
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 
@@ -12,7 +19,13 @@ public class Client extends Application {
 
     @Override
     public void start(final Stage primaryStage) throws Exception {
-
+        Flow flow = new Flow(HomeController.class);
+        FlowHandler flowHandler = flow.createHandler(new ViewFlowContext());
+        DefaultFlowContainer container = new DefaultFlowContainer();
+        StackPane pane = flowHandler.start(container);
+        primaryStage.setScene(new Scene(pane));
+        primaryStage.setOnCloseRequest(event -> System.exit(0));
+        primaryStage.show();
     }
 
 }
---
+++ src/main/java/com/metacodestudio/hotsuploader/controllers/HomeController.java
@@ -0,0 +1,7 @@
+package com.metacodestudio.hotsuploader.controllers;
+
+import io.datafx.controller.ViewController;
+
+@ViewController("../views/Home.fxml")
+public class HomeController {
+}
