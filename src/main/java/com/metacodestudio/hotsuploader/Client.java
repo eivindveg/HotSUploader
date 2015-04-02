02d9a55cb2d18041241be0ba3fdcf0ef08fa2f8a
eivindveg <eivindveg@hotmail.com>
2015-04-02 02:36:54 +0200

    Add wrapper class for JavaFX

---
+++ src/main/java/com/metacodestudio/hotsuploader/Client.java
@@ -0,0 +1,18 @@
+package com.metacodestudio.hotsuploader;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+
+public class Client extends Application {
+
+    public static void main(String[] args) {
+        Application.launch(Client.class, args);
+    }
+
+    @Override
+    public void start(final Stage primaryStage) throws Exception {
+
+    }
+
+}
