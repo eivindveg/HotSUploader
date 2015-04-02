1cb369dde549e40ece525adf26ff9dffa107b17a
eivindveg <eivindveg@hotmail.com>
2015-04-02 16:22:16 +0200

    Add reference to root file

--- src/main/java/com/metacodestudio/hotsuploader/Client.java
+++ src/main/java/com/metacodestudio/hotsuploader/Client.java
@@ -10,11 +10,42 @@ import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
+import java.io.File;
+
 
 public class Client extends Application {
 
+    private static File hotsRoot;
+
+    public static File getHotsRoot() {
+        return hotsRoot;
+    }
+
     public static void main(String[] args) {
+        hotsRoot = getRootDirectory();
         Application.launch(Client.class, args);
+    }
+
+    private static File getRootDirectory() {
+        StringBuilder builder = new StringBuilder(System.getProperty("user.home"));
+        if (isWindows()) {
+            builder.append("\\Documents\\Heroes of the Storm\\Accounts\\");
+        } else if (isMacintosh()) {
+            builder.append("/Library/Application Support/Blizzard/Heroes of the Storm/Accounts/");
+        } else {
+            throw new UnsupportedOperationException("This application requires Windows or Macintosh OSX to run");
+        }
+        return new File(builder.toString());
+    }
+
+    private static boolean isMacintosh() {
+        // TODO DETERMINE MACINTOSH PROPERTIES
+        throw new UnsupportedOperationException("Macintosh check not yet implemented");
+    }
+
+    private static boolean isWindows() {
+        String osName = System.getProperty("os.name");
+        return osName.contains("Windows");
     }
 
     @Override
