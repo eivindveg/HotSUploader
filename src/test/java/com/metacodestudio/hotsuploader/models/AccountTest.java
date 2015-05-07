bf875173d60726980f83bffcdf10a27064866d94
eivindveg <eivindveg@hotmail.com>
2015-05-07 17:54:59 +0200

    test account class as data object

--- src/test/java/com/metacodestudio/hotsuploader/models/AccountTest.java
+++ src/test/java/com/metacodestudio/hotsuploader/models/AccountTest.java
@@ -1,4 +1,23 @@
 package com.metacodestudio.hotsuploader.models;
 
+import com.metacodestudio.testutils.DataObjectTester;
+import org.junit.Before;
+import org.junit.Test;
+
 public class AccountTest {
+
+    private Account account;
+
+    @Before
+    public void setup() {
+        account = new Account();
+        account.setPlayerId(1234L);
+        account.setName("TestUser");
+    }
+
+    @Test
+    public void testClassIsValidDataObject() throws Exception {
+        DataObjectTester<Account> tester = new DataObjectTester<>(Account.class, account);
+        tester.run();
+    }
 }
