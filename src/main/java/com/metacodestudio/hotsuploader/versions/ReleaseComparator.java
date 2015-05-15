853a5991f7441dfd2972455cc09ae3e62f588c32
eivindveg <eivindveg@hotmail.com>
2015-05-15 22:05:55 +0200

    create comparator for githubrelease

--- src/main/java/com/metacodestudio/hotsuploader/versions/ReleaseComparator.java
+++ src/main/java/com/metacodestudio/hotsuploader/versions/ReleaseComparator.java
@@ -1,4 +1,23 @@
 package com.metacodestudio.hotsuploader.versions;
 
-public class ReleaseComparator {
+import java.util.Comparator;
+
+public class ReleaseComparator implements Comparator<GitHubRelease> {
+
+    public int compare(final GitHubRelease release1, final GitHubRelease release2) {
+        if (release1.isPrerelease() && !release2.isPrerelease()) {
+            return 1;
+        } else if (!release1.isPrerelease() && release2.isPrerelease()) {
+            return -1;
+        }
+
+        double release1Version = stripVersion(release1.getTagName());
+        double release2Version = stripVersion(release2.getTagName());
+
+        return Double.compare(release1Version, release2Version);
+    }
+
+    private double stripVersion(final String tagName) {
+        return Double.parseDouble(tagName.replaceAll("-\\w*", ""));
+    }
 }
