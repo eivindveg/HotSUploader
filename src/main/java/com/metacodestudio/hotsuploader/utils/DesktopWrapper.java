ea3503c5f1d068542ed691284e0d8e8910ea8f48
eivindveg <eivindveg@hotmail.com>
2015-05-19 12:57:31 +0200

    rename and refactor Linux class to DesktopWrapper

---
+++ src/main/java/com/metacodestudio/hotsuploader/utils/DesktopWrapper.java
@@ -0,0 +1,28 @@
+package com.metacodestudio.hotsuploader.utils;
+
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+
+public class DesktopWrapper {
+
+    private Desktop desktop;
+
+    public DesktopWrapper() {
+        if (Desktop.isDesktopSupported()) {
+            desktop = Desktop.getDesktop();
+        }
+    }
+
+    public void browse(URI uri) throws IOException {
+        if (usingNative()) {
+            desktop.browse(uri);
+        } else {
+            Runtime.getRuntime().exec("xdg-open " + uri.toURL());
+        }
+    }
+
+    private boolean usingNative() {
+        return desktop != null;
+    }
+}
