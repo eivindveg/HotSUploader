7788934860c67b54b2cefd67dae7f4f3b6566164
eivindveg <eivindveg@hotmail.com>
2015-05-21 13:16:44 +0200

    bump versions for release

--- src/main/java/com/metacodestudio/hotsuploader/versions/ReleaseManager.java
+++ src/main/java/com/metacodestudio/hotsuploader/versions/ReleaseManager.java
@@ -15,7 +15,7 @@ import java.util.List;
 
 public class ReleaseManager {
 
-    protected static final String CURRENT_VERSION = "1.0-RELEASE";
+    protected static final String CURRENT_VERSION = "1.1";
     protected static final String GITHUB_MAINTAINER = "eivindveg";
     protected static final String GITHUB_REPOSITORY = "HotSUploader";
     protected static final String GITHUB_RELEASES_ALL
