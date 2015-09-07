efeb5bc1ed6a9ae852ab5fcbb08e0b9f0b1cb0c8
Eivind Vegsundvåg <eivindveg@hotmail.com>
2015-09-08 01:53:33 +0200

    add FxUtils by Mateus Viccari(http://stackoverflow.com/a/27384068)

---
+++ src/main/java/com/metacodestudio/hotsuploader/utils/FXUtils.java
@@ -0,0 +1,100 @@
+package com.metacodestudio.hotsuploader.utils;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
+import javafx.scene.control.ComboBox;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+
+/**
+ * @author Mateus Viccari
+ */
+public class FXUtils {
+
+    public enum AutoCompleteMode {
+        STARTS_WITH,CONTAINING,;
+    }
+
+    public static<T> void autoCompleteComboBox(ComboBox<T> comboBox, AutoCompleteMode mode) {
+        ObservableList<T> data = comboBox.getItems();
+
+        comboBox.setEditable(true);
+        comboBox.getEditor().focusedProperty().addListener(observable -> {
+            if (comboBox.getSelectionModel().getSelectedIndex() < 0) {
+                comboBox.getEditor().setText(null);
+            }
+        });
+        comboBox.addEventHandler(KeyEvent.KEY_PRESSED, t -> comboBox.hide());
+        comboBox.addEventHandler(KeyEvent.KEY_RELEASED, new EventHandler<KeyEvent>() {
+
+            private boolean moveCaretToPos = false;
+            private int caretPos;
+
+            @Override
+            public void handle(KeyEvent event) {
+                if (event.getCode() == KeyCode.UP) {
+                    caretPos = -1;
+                    moveCaret(comboBox.getEditor().getText().length());
+                    return;
+                } else if (event.getCode() == KeyCode.DOWN) {
+                    if (!comboBox.isShowing()) {
+                        comboBox.show();
+                    }
+                    caretPos = -1;
+                    moveCaret(comboBox.getEditor().getText().length());
+                    return;
+                } else if (event.getCode() == KeyCode.BACK_SPACE) {
+                    moveCaretToPos = true;
+                    caretPos = comboBox.getEditor().getCaretPosition();
+                } else if (event.getCode() == KeyCode.DELETE) {
+                    moveCaretToPos = true;
+                    caretPos = comboBox.getEditor().getCaretPosition();
+                }
+
+                if (event.getCode() == KeyCode.RIGHT || event.getCode() == KeyCode.LEFT
+                        || event.isControlDown() || event.getCode() == KeyCode.HOME
+                        || event.getCode() == KeyCode.END || event.getCode() == KeyCode.TAB) {
+                    return;
+                }
+
+                ObservableList<T> list = FXCollections.observableArrayList();
+                for (T aData : data) {
+                    if (mode.equals(AutoCompleteMode.STARTS_WITH) && aData.toString().toLowerCase().startsWith(comboBox.getEditor().getText().toLowerCase())) {
+                        list.add(aData);
+                    } else if (mode.equals(AutoCompleteMode.CONTAINING) && aData.toString().toLowerCase().contains(comboBox.getEditor().getText().toLowerCase())) {
+                        list.add(aData);
+                    }
+                }
+                String t = comboBox.getEditor().getText();
+
+                comboBox.setItems(list);
+                comboBox.getEditor().setText(t);
+                if (!moveCaretToPos) {
+                    caretPos = -1;
+                }
+                moveCaret(t.length());
+                if (!list.isEmpty()) {
+                    comboBox.show();
+                }
+            }
+
+            private void moveCaret(int textLength) {
+                if (caretPos == -1) {
+                    comboBox.getEditor().positionCaret(textLength);
+                } else {
+                    comboBox.getEditor().positionCaret(caretPos);
+                }
+                moveCaretToPos = false;
+            }
+        });
+    }
+
+    public static<T> T getComboBoxValue(ComboBox<T> comboBox){
+        if (comboBox.getSelectionModel().getSelectedIndex() < 0) {
+            return null;
+        } else {
+            return comboBox.getItems().get(comboBox.getSelectionModel().getSelectedIndex());
+        }
+    }
+}
