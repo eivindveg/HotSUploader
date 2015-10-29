7800817b8218dd8fe8ff45b7f7a9296009c6e602
eivindveg <eivindveg@hotmail.com>
2015-10-29 13:54:59 +0100

    implement getApplicationHome for WindowsService

--- src/main/java/ninja/eivind/hotsreplayuploader/services/WindowsService.java
+++ src/main/java/ninja/eivind/hotsreplayuploader/services/WindowsService.java
@@ -1,13 +1,17 @@
 package ninja.eivind.hotsreplayuploader.services;
 
 import java.awt.*;
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.net.URI;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class WindowsService implements PlatformService {
 
     private Desktop desktop;
+
+    private Pattern pathPattern = Pattern.compile("[A-Z]:(\\\\|\\w+| )+");
+    private File documentsHome;
 
     public WindowsService() {
         desktop = Desktop.getDesktop();
@@ -15,7 +19,49 @@ public class WindowsService implements PlatformService {
 
     @Override
     public File getApplicationHome() {
-        return null;
+        if (documentsHome == null) {
+            try {
+                documentsHome = findMyDocuments();
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return new File(documentsHome + "\\" + APPLICATION_DIRECTORY_NAME);
+    }
+
+    private File findMyDocuments() throws FileNotFoundException {
+        Process p = null;
+        String myDocuments = null;
+        try {
+            p = Runtime.getRuntime().exec("reg query \"HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Shell Folders\" /v personal");
+            p.waitFor();
+
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()))) {
+                StringBuilder builder = new StringBuilder();
+                reader.lines().forEach(builder::append);
+                final String[] values = builder.toString().trim().split("\\s\\s+");
+                for (final String value : values) {
+                    System.out.println(value);
+                    Matcher matcher = pathPattern.matcher(value);
+                    if (matcher.matches()) {
+                        myDocuments = matcher.group();
+                        break;
+                    }
+                }
+            }
+
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (p != null) {
+                p.destroy();
+            }
+        }
+
+        if (myDocuments == null) {
+            throw new FileNotFoundException("Could not locate Documents folder");
+        }
+        return new File(myDocuments);
     }
 
     @Override
