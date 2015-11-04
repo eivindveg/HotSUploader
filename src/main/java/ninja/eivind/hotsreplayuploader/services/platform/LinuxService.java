0708a7caef9984237d469a4fdbecece706093be8
eivindveg <eivindveg@hotmail.com>
2015-11-04 14:34:02 +0100

    apply desktop usage for linux versions that claim to support it(untested)

--- src/main/java/ninja/eivind/hotsreplayuploader/services/platform/LinuxService.java
+++ src/main/java/ninja/eivind/hotsreplayuploader/services/platform/LinuxService.java
@@ -1,10 +1,19 @@
 package ninja.eivind.hotsreplayuploader.services.platform;
 
+import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 import java.net.URI;
 
 public class LinuxService implements PlatformService {
+
+    private Desktop desktop;
+
+    public LinuxService() {
+        if(Desktop.isDesktopSupported()) {
+            desktop = Desktop.getDesktop();
+        }
+    }
     @Override
     public File getApplicationHome() {
         return new File(USER_HOME, APPLICATION_DIRECTORY_NAME);
@@ -17,6 +26,10 @@ public class LinuxService implements PlatformService {
 
     @Override
     public void browse(final URI uri) throws IOException {
-        Runtime.getRuntime().exec("xdg-open " + uri.toURL());
+        if (Desktop.isDesktopSupported()) {
+            desktop.browse(uri);
+        } else {
+            Runtime.getRuntime().exec("xdg-open " + uri.toURL());
+        }
     }
 }
