e9c663e0eaa7bb45c80a968d34dbd0ff120a8d2d
eivindveg <eivindveg@hotmail.com>
2015-11-04 16:40:38 +0100

    remove import

--- src/main/java/ninja/eivind/hotsreplayuploader/services/UploaderService.java
+++ src/main/java/ninja/eivind/hotsreplayuploader/services/UploaderService.java
@@ -1,6 +1,5 @@
 package ninja.eivind.hotsreplayuploader.services;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 import javafx.collections.FXCollections;
