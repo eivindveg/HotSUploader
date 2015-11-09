e38686ed441c02dc62f34245eed19d4dbbaf6a3a
Philipp Gruhn <philipp.gruhn@googlemail.com>
2015-11-09 21:42:39 +0100

    moved task to concurrent package

--- src/main/java/ninja/eivind/hotsreplayuploader/concurrent/tasks/UploadTask.java
+++ src/main/java/ninja/eivind/hotsreplayuploader/concurrent/tasks/UploadTask.java
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package ninja.eivind.hotsreplayuploader.files;
+package ninja.eivind.hotsreplayuploader.concurrent.tasks;
 
 import javafx.concurrent.Task;
 import ninja.eivind.hotsreplayuploader.models.ReplayFile;
