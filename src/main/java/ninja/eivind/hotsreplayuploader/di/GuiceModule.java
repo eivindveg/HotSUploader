cc04921b1ecda62cb89fa72a0d49d35c61825d29
Eivind Vegsundvåg <eivindveg@hotmail.com>
2015-11-25 16:22:52 +0100

    bind uploaderservice as singleton
    this causes it to prepare its lists and queues before the controller loads by loading it during the preloader stage. This results in faster overall launch and removes the timing gap between preloading and window display

--- src/main/java/ninja/eivind/hotsreplayuploader/di/GuiceModule.java
+++ src/main/java/ninja/eivind/hotsreplayuploader/di/GuiceModule.java
@@ -25,6 +25,7 @@ import com.google.inject.spi.TypeEncounter;
 import com.google.inject.spi.TypeListener;
 import com.j256.ormlite.support.ConnectionSource;
 import ninja.eivind.hotsreplayuploader.repositories.*;
+import ninja.eivind.hotsreplayuploader.services.UploaderService;
 import ninja.eivind.hotsreplayuploader.services.platform.PlatformService;
 import ninja.eivind.hotsreplayuploader.services.platform.PlatformServiceProvider;
 import org.slf4j.Logger;
@@ -40,6 +41,7 @@ public class GuiceModule extends AbstractModule {
     protected void configure() {
         LOG.info("Instantiating IoC Container");
         bind(PlatformService.class).toProvider(new PlatformServiceProvider()).asEagerSingleton();
+        bind(UploaderService.class).asEagerSingleton();
         bind(FileRepository.class).to(OrmLiteFileRepository.class).asEagerSingleton();
         bind(DataSource.class).toProvider(DataSourceProvider.class).asEagerSingleton();
         bind(ConnectionSource.class).toProvider(ConnectionSourceProvider.class).asEagerSingleton();
