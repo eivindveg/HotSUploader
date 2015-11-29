c80428ea2bfc475d0eed504014f7e3cd8af967d4
eivindveg <eivindveg@hotmail.com>
2015-11-29 02:36:50 +0100

    add missing javadoc parameters

--- src/main/java/ninja/eivind/hotsreplayuploader/scene/control/ImageButton.java
+++ src/main/java/ninja/eivind/hotsreplayuploader/scene/control/ImageButton.java
@@ -33,8 +33,8 @@ public class ImageButton extends Button {
     /**
      * Create an {@link ImageButton} with the specified height and width.
      *
-     * @param height
-     * @param width
+     * @param height height of the {@link Button}
+     * @param width width of the {@link Button}
      */
     public ImageButton(double height, double width) {
         this.height = height;
