e413b16b7d2df851f372e043ff7c4541023341b3
Philipp Gruhn <philipp.gruhn@googlemail.com>
2015-12-07 13:00:18 +0100

    workaround for #91 - writes new replays to the db - refreshes after an update

--- src/main/java/ninja/eivind/hotsreplayuploader/services/UploaderService.java
+++ src/main/java/ninja/eivind/hotsreplayuploader/services/UploaderService.java
@@ -71,7 +71,7 @@ public class UploaderService extends ScheduledService<ReplayFile> implements Ini
     public void initialize() {
         LOG.info("Initializing " + getClass().getSimpleName());
         watcher.addFileListener(file -> {
-            fileRepository.deleteByFileName(file);
+            fileRepository.updateReplay(file);
             files.add(file);
             uploadQueue.add(file);
         });
