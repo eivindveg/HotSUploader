65c936b874acfd059ca4304b564da08b14a097ce
Chris Rogus <rogusdev@gmail.com>
2016-07-20 17:52:03 -0400

    Sort accounts by max last modified timestamp on replays for that account, resolves #118

--- src/main/java/ninja/eivind/hotsreplayuploader/utils/StormHandler.java
+++ src/main/java/ninja/eivind/hotsreplayuploader/utils/StormHandler.java
@@ -57,13 +57,13 @@ public class StormHandler {
      * @return {@link List} of directories or an empty {@link List}
      */
     public List<File> getReplayDirectories() {
-        final List<File> replayDirectories = new ArrayList<>();
+        return getAccountDirectories().stream()
+                .map(StormHandler::getReplayDirectory)
+                .collect(Collectors.toList());
+    }
 
-        getAccountDirectories().stream()
-                    .map(folder -> new File(folder, "Replays"))
-                    .map(folder -> new File(folder, "Multiplayer"))
-                    .forEach(replayDirectories::add);
-        return replayDirectories;
+    private static File getReplayDirectory(File dir) {
+        return new File(dir, "Replays" + File.separator + "Multiplayer");
     }
 
     /**
@@ -99,6 +99,11 @@ public class StormHandler {
         }
     }
 
+    private static long maxLastModified(File dir) {
+        return Arrays.stream(getReplayDirectory(dir).listFiles())
+                .mapToLong(f -> f.lastModified()).max().orElse(Long.MIN_VALUE);
+    }
+
     /**
      * Retrieves a {@link List} of {@link File}s, each containing files for a specific {@link Account}.
      * @return {@link List} of directories or an empty {@link List}
@@ -114,6 +119,8 @@ public class StormHandler {
             Arrays.stream(hotsFolders).forEach(hotsAccounts::add);
         }
 
-        return hotsAccounts;
+         return hotsAccounts.stream()
+                .sorted((f1, f2) -> Long.compare(maxLastModified(f2), maxLastModified(f1)))
+                .collect(Collectors.toList());
     }
 }
