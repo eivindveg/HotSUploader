11ed0721101700925271b481b611d08a81c9f540
Eivind Vegsundvåg <eivindveg@hotmail.com>
2016-08-29 21:22:04 +0200

    add security warning on aws credentials

--- src/main/java/ninja/eivind/hotsreplayuploader/providers/hotslogs/HotsLogsAWSCredentials.java
+++ src/main/java/ninja/eivind/hotsreplayuploader/providers/hotslogs/HotsLogsAWSCredentials.java
@@ -17,6 +17,17 @@ package ninja.eivind.hotsreplayuploader.providers.hotslogs;
 import com.amazonaws.auth.BasicAWSCredentials;
 import org.springframework.stereotype.Component;
 
+/**
+ * The AWS credentials for the public HotsLogs S3 bucket.
+ * DEAR READER: These are public, and this is very much intended. We understand your concerns about security, but given
+ * the current technological environment, it is impossible to hide this from anyone who wants to inspect the memory of
+ * the application. In fact, Java debuggers have gotten so advanced that it is trivial. Therefore, the security of this
+ * key rests solely on proper restrictions having been placed on it. We, the developers, have not personally verified
+ * this, but trust the developer of HotsLogs has done the correct amount of work.
+ *
+ * If you wish to further inquire about ideas for improving the security model of this layer, please email
+ * {@literal mail@eivind.ninja} with {@literal admin@hotslogs.com} on CC.
+ */
 @Component("hotslogs-s3credentials")
 public class HotsLogsAWSCredentials extends BasicAWSCredentials {
 
