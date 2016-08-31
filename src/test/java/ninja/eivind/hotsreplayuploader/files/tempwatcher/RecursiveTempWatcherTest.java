eb3fdcd01af48940774ae1f10920965c4d6be106
Eivind Vegsundvåg <eivindveg@hotmail.com>
2016-08-31 17:12:48 +0200

    fail unimplemented tests

--- src/test/java/ninja/eivind/hotsreplayuploader/files/tempwatcher/RecursiveTempWatcherTest.java
+++ src/test/java/ninja/eivind/hotsreplayuploader/files/tempwatcher/RecursiveTempWatcherTest.java
@@ -7,22 +7,22 @@ import static org.junit.Assert.*;
 public class RecursiveTempWatcherTest {
     @Test
     public void testStart() throws Exception {
-
+        fail("Test not implemented");
     }
 
     @Test
     public void testStop() throws Exception {
-
+        fail("Test not implemented");
     }
 
     @Test
     public void testSetCallback() throws Exception {
-
+        fail("Test not implemented");
     }
 
     @Test
     public void testSetChildCount() throws Exception {
-
+        fail("Test not implemented");
     }
 
 }
